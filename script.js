59cd848b99e0505d471050dc119028278f6b0c84
Lipe3es <74935855+Lipe3es@users.noreply.github.com>
2021-10-14 16:31:07 -0300

    site

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function imc() {
     if (nome !== '' && altura !== '' && peso !== '') {
 
         const valorIMC = (peso / (altura ** 2)).toFixed(2);
+        const pesoMinimo = (18.5 * (altura ** 2)).toFixed(2);
+        const pesoMaximo = (25 * (altura ** 2)).toFixed(2);
 
         let classificacao = '';
 
@@ -27,7 +29,8 @@ function imc() {
             classificacao = 'com obesidade grau III.';
         }
 
-        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}`;
+        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}. 
+        Procure manter o seu peso entre ${pesoMinimo} e ${pesoMaximo}kg.`;
 
     } else {
         resultado.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
