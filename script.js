13a41aace3892df608d8a03e3bddfed1e55e996d
Lipe3es <74935855+Lipe3es@users.noreply.github.com>
2021-10-14 16:01:11 -0300

    site nova

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-document.getElementById("calcular").onclick = function() {
-    var nome = document.getElementById('nome').value
-    var altura = document.getElementById('altura').value
-    var peso = document.getElementById('peso').value
+const calcular = document.getElementById('calcular');
 
-    if (altura != "" && peso != "" && nome != "") {
-        var imc = peso / (altura * altura)
-        imc = imc.toFixed(2);
 
-        var pesoMinimo = 18.5 * (altura * altura)
-        pesoMinimo = pesoMinimo.toFixed(0);
-        var pesoMaximo = 27.9 * (altura * altura)
-        pesoMaximo = pesoMaximo.toFixed(0);
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
-        var resultado = document.getElementById('resultado')
-        resultado.style.display = "block"
+    if (nome !== '' && altura !== '' && peso !== '') {
 
-        var txt_situacao = document.getElementById('situacao')
-        var txt_imc = document.getElementById('imc')
-        var txt_nome = document.getElementById('nome')
-        var txt_pesoIdeal = document.getElementById('pesoIdeal')
+        const valorIMC = (peso / (altura ** 2)).toFixed(2);
 
-        if (imc < 20) {
-            txt_situacao.innerHTML = "abaixo do peso!"
-        } else if (imc > 20 && imc <= 25) {
-            txt_situacao.innerHTML = "no Peso Ideal!"
-        } else if (imc > 25 && imc <= 30) {
-            txt_situacao.innerHTML = "no Sobrepeso!"
-        } else if (imc > 30 && imc <= 35) {
-            txt_situacao.innerHTML = "em Obesidade Moderada!"
-        } else if (imc > 35 && imc <= 40) {
-            txt_situacao.innerHTML = "em Obesidade Severa!"
-        } else if (imc > 40 && imc <= 50) {
-            txt_situacao.innerHTML = "em Obesidade Morbida!"
+        let classificacao = '';
+
+        if (valorIMC < 18.5) {
+            classificacao = 'abaixo do peso.';
+        } else if (valorIMC < 25) {
+            classificacao = 'com o peso ideal. Parabéns!';
+        } else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso.';
+        } else if (valorIMC < 35) {
+            classificacao = 'com obesidade grau I.';
+        } else if (valorIMC < 40) {
+            classificacao = 'com obesidade grau II.';
         } else {
-            txt_situacao.innerHTML = "muito Gordo"
+            classificacao = 'com obesidade grau III.';
         }
 
-        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${imc}. Atualmente, você está ${situacao}`;
+        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao}`;
 
     } else {
-        alert("Preencha todos os campos")
+        resultado.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
     }
-};
 
-document.getElementById('limpar').onclick = function() {
-    var resultado = document.getElementById('resultado')
-    resultado.style.display = "none"
-};
+}
+calcular.addEventListener('click', imc);
